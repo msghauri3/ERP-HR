6b52096c60d4ce53f8439a4b9025452669163150
msghauri3 <msghauri3@yahoo.com>
2025-08-09 12:44:19 +0500

    Initial commit

---
+++ HR/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using HR.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 800 },
+            new Product { Id = 2, Name = "Phone", Price = 500 }
+        };
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll()
+        {
+            return products;
+        }
+
+
+
+        // GET: api/products/1
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+            return product;
+        }
+
+
+        // POST: api/products
+        [HttpPost]
+        public ActionResult<Product> Create(Product product)
+        {
+            product.Id = products.Max(p => p.Id) + 1;
+            products.Add(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+
+
+
+        // DELETE: api/products/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+
+            products.Remove(product);
+            return NoContent();
+        }
+
+
+
+    }
+}
